59562c92087e4fec2dfcb00e037bec75ca9ccab0
Yusuf Akin <kutluhanakin@gmail.com>
2022-04-20 16:43:02 -0400

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const apiKey = "a3a2325f06c657296bb6565f0f1ab80d";
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [zipcode, setCity] = useState("");
+  const time = new Date().toDateString();
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+        });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="input"
+        placeholder="Enter zip code"
+        onChange={(e) => setCity(e.target.value)}
+        value={zipcode}
+        onKeyPress={getWeather}
+      />
+      <div className="button1">
+      </div>
+
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p className="centered">
+            Welcome to the weather app. Enter your zipcode and hit enter
+          </p>
+        </div>
+      ) : (
+        <div className="weather-data">
+          <p className="smalldata">{time}</p>
+          <p className="city">{weatherData.name}</p>
+          <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="" />
+          <p className="temp">{Math.round(weatherData.main.temp)}F</p>
+          <p className="weather">{weatherData.weather[0].main}</p>
+          <p className="smalldata">Feels like: {weatherData.main.feels_like}</p>
+          <p className="smalldata">
+            Min Temperature:{weatherData.main.temp_min}
+          </p>
+          <p className="smalldata">
+            Max Temperature: {weatherData.main.temp_max}
+          </p>
+          <p className="smalldata">Humidity: {weatherData.main.humidity}</p>
+          
+        </div>
+      )}
     </div>
   );
 }
